f0eba4d3c38fc39e33c109f96d4862b42f709afa
Ryan Stong <rvanstong@gmail.com>
2017-06-14 07:52:21 -0700

    adds in initial functionality to check if north; displays image rn, but should activate a vibration motor when facing north

--- true-north.py
+++ true-north.py
@@ -1,5 +1,14 @@
 from microbit import *
 
+compass.calibrate()
+
+def check_if_north()
+    if compass.is_calibrated()
+        return compass.heading()
+
+def notify()
+    if check_if_north() == 0
+        display.show(Image.HAPPY)
 
 while True:
-
+    notify()
