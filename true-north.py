1f914ea06c83ab553c0516445020ce6d57e64fd0
Ryan Stong <rvanstong@gmail.com>
2017-06-16 08:05:08 -0700

    fixes syntax errors

--- true-north.py
+++ true-north.py
@@ -2,14 +2,10 @@ from microbit import *
 
 compass.calibrate()
 
-def check_if_north()
-    if compass.is_calibrated()
-        return compass.heading()
-
-def notify_if_north()
-    if check_if_north() == 0
+def notify_if_north():
+    if compass.heading == 0:
         display.show(Image.HEART)
-    elif
+    elif:
         display.show(Image.ANGRY)
 
 
